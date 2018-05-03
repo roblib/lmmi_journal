b22adf60cd71ba4d3ebc98ebf4ca8fea9beeb30f
rdrew <robdrewupei@gmail.com>
2018-05-03 11:47:35 -0300

    .

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  browserSync: {
+    hostname: "drupal8.test",
+    port: 8080,
+    openAutomatically: true,
+    reloadDelay: 50,
+    injectChanges: true,
+  },
+
+  drush: {
+    enabled: false,
+    alias: {
+      css_js: 'drush @SITE-ALIAS cc css-js',
+      cr: 'drush @SITE-ALIAS cr'
+    }
+  },
+
+  twig: {
+    useCache: true
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,89 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell');
+var notify = require('gulp-notify');
 var browserSync = require('browser-sync').create();
-var $    = require('gulp-load-plugins')();
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var fs = require("fs");
+var runSequence = require('run-sequence');
+var config = require("./example.config");
 
-var sassPaths = [
-  //'node_modules/foundation-sites/scss',
-  //'node_modules/motion-ui/src'
-];
+/**
+ * If config.js exists, load that config for overriding certain values below.
+ */
+function loadConfig() {
+  if (fs.existsSync(__dirname + "/./config.js")) {
+    config = {};
+    config = require("./config");
+  }
 
-gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+  return config;
+}
+
+loadConfig();
+
+/**
+ * This task generates CSS from all SCSS files and compresses them down.
+ */
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      noCache: true,
+      outputStyle: "compressed",
+      lineNumbers: false,
+      loadPath: './css/*',
+      sourceMap: true
+    })).on('error', function(error) {
+      gutil.log(error);
+      this.emit('end');
     })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./css'))
+    .pipe(notify({
+      title: "SASS Compiled",
+      message: "All SASS files have been recompiled to CSS.",
+      onLast: true
+    }));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+
+/**
+ * Define a task to spawn Browser Sync.
+ * Options are defaulted, but can be overridden within your config.js file.
+ */
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    files: ['css/**/*.css', 'js/**/*.js'],
+    port: config.browserSync.port,
+    proxy: config.browserSync.hostname,
+    open: config.browserSync.openAutomatically,
+    reloadDelay: config.browserSync.reloadDelay,
+    injectChanges: config.browserSync.injectChanges
+  });
 });
+
+/**
+ * Define a task to be called to instruct browser sync to reload.
+ */
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+
+/**
+ * Defines the watcher task.
+ */
+gulp.task('watch', function() {
+  // watch scss for changes and clear drupal theme cache on change
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+
+  // watch js for changes and clear drupal theme cache on change
+  //gulp.watch(['js/js-src/**/*.js'], ['compress', 'drush:cc']);
+
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
